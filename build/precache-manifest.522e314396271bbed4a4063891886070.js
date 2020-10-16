3c5297187a31b97e9d4c56eeabab5333ccd1c3f5
johann <584971501@qq.com>
2020-10-16 22:31:38 +0800

    fiex route ii

---
+++ build/precache-manifest.522e314396271bbed4a4063891886070.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b105f648140c9dc9f92528c3a06e66b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "febbca2f9cfcb24f6cd9",
+    "url": "/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "b9cf243e4a87bfeac32b",
+    "url": "/static/css/main.12da3f32.chunk.css"
+  },
+  {
+    "revision": "febbca2f9cfcb24f6cd9",
+    "url": "/static/js/2.5c955e80.chunk.js"
+  },
+  {
+    "revision": "3043e3e727d80f353557f855ab1ce92b",
+    "url": "/static/js/2.5c955e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9cf243e4a87bfeac32b",
+    "url": "/static/js/main.8db8617e.chunk.js"
+  },
+  {
+    "revision": "5137aa6833cb32b38cdc",
+    "url": "/static/js/runtime-main.e928216b.js"
+  }
+]);
